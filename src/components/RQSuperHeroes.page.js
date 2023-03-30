cbc7ef067fd868755d7002825db485174ca198bc
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-03-30 10:55:38 +0100

    useQuery

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,23 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes1');
+};
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { isLoading, data, isError, error } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes
+  );
+  if (isLoading) return <h2>Is loading</h2>;
+  if (isError) return <h2>{error.message}</h2>;
+  return (
+    <>
+      {' '}
+      <h2>React Query Super Heroes Page</h2>
+      {data.data?.map((hero) => (
+        <div key={hero.id}>{hero.name}</div>
+      ))}
+    </>
+  );
+};
